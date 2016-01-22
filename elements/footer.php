4d5658e5032e5069cccbb851db82c1aac7bae899
Madelyn Perkins <jellybean101@Madelyn-Perkinss-MacBook-Pro.local>
2016-01-21 19:35:46 -0500

    included footer.php

---
+++ elements/footer.php
@@ -0,0 +1,4 @@
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,5 @@
 		<?php include ('elements/navigation.php'); ?>
 		<h1>Land of Finnick</h1>
 	</div>
-	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-</body>
-</html>
+		
+<?php include ('elements/footer.php'); ?>
