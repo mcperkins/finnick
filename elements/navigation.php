3d9b4468f2f45d55ea72dccd5baa39fd3be4b9b0
Madelyn Perkins <jellybean101@Madelyn-Perkinss-MacBook-Pro.local>
2016-01-21 19:48:12 -0500

    include elements on all pages

--- elements/navigation.php
+++ elements/navigation.php
@@ -1,9 +1,9 @@
 <header class="home-header">
 			<nav>
 				<ul>
-					<li><a href="/about.html">About</a></li>
-					<li><a href="/gallery.html">Gallery</a></li>
-					<li><a href="/contact.html">Contact</a></li>
+					<li><a href="/finnick/about.php">About</a></li>
+					<li><a href="/finnick/gallery.php">Gallery</a></li>
+					<li><a href="/finnick/contact.php">Contact</a></li>
 				</ul>
 			</nav>
 		</header>
